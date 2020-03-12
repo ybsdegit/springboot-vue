576b123d42f4b1e8acc16609f502ca8b68e61f7d
ybsdegit <ybsdeyx@163.com>
2020-03-12 18:36:10 +0800

    创建秒杀项目

---
+++ spring-vue-seckill/src/test/java/com/ybs/seckil/SeckilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ybs.seckil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeckilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
