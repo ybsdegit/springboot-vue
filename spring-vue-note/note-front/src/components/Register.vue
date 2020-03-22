cdc028734e62c277ad61907b200483f07fa82733
ybsdegit <ybsdeyx@163.com>
2020-03-22 20:24:04 +0800

    springboot-vue 笔记项目

---
+++ spring-vue-note/note-front/src/components/Register.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <el-form ref="form" :model="registerForm" label-width="80px">
+            <el-form-item>
+                <el-input placeholder="请输入用户名" v-model="registerForm.username"></el-input>
+            </el-form-item>
+
+            <el-form-item>
+                <el-input placeholder="请输入密码" v-model="registerForm.password" show-password></el-input>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" @click="onSubmit">提交</el-button>
+                <el-button>取消</el-button>
+            </el-form-item>
+        </el-form>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Register",
+        data() {
+            return {
+                registerForm: {
+                    username: '',
+                    password: '',
+                }
+            }
+        },
+        methods: {
+            onSubmit() {
+                console.log('submit!');
+            }
+        }
+    }
+</script>
+
+
+<style scoped>
+
+</style>
