cac0c63d8c16832a676b2c8b4fe6050398f211b7
ybsdegit <ybsdeyx@163.com>
2020-06-01 22:00:51 +0800

    base config

--- springboot-vue-phonestore/phone-store-front/src/views/Home.vue
+++ springboot-vue-phonestore/phone-store-front/src/views/Home.vue
@@ -1,18 +1,45 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <van-tabs v-model="active">
+      <van-tab title="标签 1">内容 1</van-tab>
+      <van-tab title="标签 2">内容 2</van-tab>
+      <van-tab title="标签 3">内容 3</van-tab>
+      <van-tab title="标签 4">内容 4</van-tab>
+    </van-tabs>
+
+    <van-card
+      num="2"
+      price="2.00"
+      desc="描述信息"
+      title="商品标题"
+      thumb="https://img.yzcdn.cn/vant/ipad.jpeg"
+    >
+      <template #tags>
+        <van-tag plain type="danger">标签</van-tag>
+        <van-tag plain type="danger">标签</van-tag>
+      </template>
+      <template #footer>
+        <van-button round type="info" size="mini">按钮</van-button>
+      </template>
+    </van-card>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  components: {},
+  data() {
+    return {
+      active: 2,
+      categories: '',
+      phones: '',
+      show: true,
+      sku: '',
+      goods: '',
+    }
+  },
 }
 </script>
--- springboot-vue-phonestore/phone-store-front/vue.config.js
+++ springboot-vue-phonestore/phone-store-front/vue.config.js
@@ -1,16 +1,5 @@
-const BASE_URL = process.env.NODE_ENV === 'production' ? 'iview-admin' : '/'
-
-const path = require('path')
-const resolve = dir => path.join(__dirname, dir)
-
 module.exports = {
   lintOnSave: true,
-  publicPath: BASE_URL,
-  chainWebpack: config => {
-    config.resolve.alias
-      .set('@', resolve('src'))
-      .set('_c', resolve('src/components'))
-  },
 
   // 打包时不生成 .map 文件 加快打包速度
   productionSourceMap: false,
